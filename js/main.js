61f35251d244fc4789b28f92643b8ab8070e4832
erynjones <eryn.jones16@hotmail.com>
2016-03-08 16:40:48 -0500

    trying to make it work

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 var $newH1 = $('<h1>');
+var $belowveggies = $('<ul>');
+var $aboveH1 = $('<h1>');
+var $aboveveggies = $('<ul>');
+
 $newH1.html('Below ground veggies');
 $('body').prepend($newH1);
+
+
+$('body').prepend($belowveggies);
+
+$aboveH1.html('Above ground veggies');
+$('body').append($aboveH1);
+
+
+$('body').append($aboveveggies);
+
+$('ul li').each(function (aboveH1) {
+  $(above).prepend('aboveveggies')
+});
+
+$('ul li').each(function (newH1) {
+  $(below).append('belowveggies')
+});
